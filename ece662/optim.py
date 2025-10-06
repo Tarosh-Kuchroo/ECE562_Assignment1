b65e1be2366dec91bdac7e6d1aa4eb3642b2cded
Tarosh-Kuchroo <tkuchroo@umass.edu>
2025-10-05 22:05:20 -0400

    added sgd_momentum and optim.py

--- ece662/optim.py
+++ ece662/optim.py
@@ -68,8 +68,11 @@ def sgd_momentum(w, dw, config=None):
     # the next_w variable. You should also use and update the velocity v.     #
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    momentum = config["momentum"] #Basically pulls the momentum coeff
+    lr = config["learning_rate"] #grabs tthe learning rate
 
-    pass
+    v = momentum * v - lr * dw #updating the velocity vector
+    next_w = w + v #This applies the velocity to the weights
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
